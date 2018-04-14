614f88bd5cffbd49d6febe9b60371d389583fff2
Tomasz Długokiński <tomaszdlugokinski@gmail.com>
2018-04-14 12:29:25 +0200

    Dodaj pliki projektów.

---
+++ Find My Food/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Find_My_Food.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
